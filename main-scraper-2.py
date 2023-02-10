1b707e7298755ba89b9ca0b6f9fe96ca604818b4
nick-vieira <62452310+nick-vieira@users.noreply.github.com>
2023-02-10 17:47:59 -0500

    Add files via upload
    update to linkedin scraper

--- main-scraper-2.py
+++ main-scraper-2.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 file = open('linkedin-jobs.csv', 'a')
 writer = csv.writer(file)
-writer.writerow(['Title', 'Company', 'Location', 'Apply'])
+writer.writerow(['Title', 'Company', 'Location', 'Seniority', 'Employment Type', 'Apply'])
 
 def linkedin_scraper(webpage, page_number):
   next_page = webpage + str(page_number)
@@ -21,20 +21,45 @@ def linkedin_scraper(webpage, page_number):
     job_company = job.find('h4', class_='base-search-card__subtitle').text.strip()
     job_location = job.find('span', class_='job-search-card__location').text.strip()
     job_link = job.find('a', class_='base-card__full-link')['href']
+
     writer.writerow([
     job_title.encode('utf-8'),
     job_company.encode('utf-8'),
     job_location.encode('utf-8'),
     job_link.encode('utf-8')
-  ])
+    ])
 
+  # job_cards = soup.find_all('section',{"class" : "core-section-container my-3 description"})
+  # for card in job_cards:
+  #   job_seniority = card.find_all('span', class_="description__job-criteria-text description__job-criteria-text--criteria")
+  #   job_employment_type = card.find_all('span', class_='description__job-criteria-text description__job-criteria-text--criteria').text.strip()
+
+  #   writer.writerow([
+  #   job_seniority.encode('utf-8'),
+  #   job_employment_type.encode('utf-8'),
+  #   ])
+    
   print('Data updated')
 
-  if page_number < 25:
-    page_number = page_number + 25
+  if page_number < 10:
+    page_number = page_number + 1
     linkedin_scraper(webpage, page_number)
   else:
     file.close()
     print('File closed')
 
-linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=data%20analyst&location=Toronto%2C%20Ontario%2C%20Canada&geoId=100025096&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=data%20analyst&location=canada&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=software%20engineer&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Sales&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Graphic%20Designer&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Marketing%20Specialist&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Sales%20Management&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Sales%20Executive&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Software%20Development&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Structural%20Engineering&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Business%20Analysis&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Data%20Science&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Project%20Management&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Sales%20Engineer&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Database%20Administration&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
+# linkedin_scraper('https://www.linkedin.com/jobs/search?keywords=Data%20Specialist&location=Canada&geoId=101174742&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0', 0)
